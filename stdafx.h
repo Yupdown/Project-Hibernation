71d8cc297343507a418d944dda97ad866e2453fa
Yup <do.yupdown@gmail.com>
2025-12-06 15:17:20 +0900

    Project initialization as Vulkan

---
+++ stdafx.h
@@ -0,0 +1,73 @@
+#pragma once
+
+// C++ Standard Library
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <concepts>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <exception>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <set>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <random>
+#include <span>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+// Windows Library
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#include <windows.h>
+#include <wrl.h>
+#include <shellapi.h>
+#include <shlwapi.h>
+#include <comdef.h>
+#include <commdlg.h>
+#include <wincodec.h>
+#include <wincodecsdk.h>
+
+// Vulkan Library
+#define VK_NO_PROTOTYPES
+#define VMA_STATIC_VULKAN_FUNCTIONS 0
+#define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
+#include <volk.h>
+#include <vulkan/vulkan.hpp>
+#include <VkBootstrap.h>
+#include <vma/vk_mem_alloc.h>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+// ImGui Library
+#define IMGUI_IMPL_VULKAN_NO_PROTOTYPES
+#include <imgui.h>
+#include <implot.h>
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_vulkan.h"
